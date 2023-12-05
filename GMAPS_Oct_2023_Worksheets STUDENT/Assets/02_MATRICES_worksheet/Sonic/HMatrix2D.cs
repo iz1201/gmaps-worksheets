dbd07684e42d10910806e859eec4257529a67057
iz1201 <isaaclzr123@hotmail.com>
2023-12-06 00:06:20 +0800

    HMatrix2D and TransformMesh update

--- GMAPS_Oct_2023_Worksheets STUDENT/Assets/02_MATRICES_worksheet/Sonic/HMatrix2D.cs
+++ GMAPS_Oct_2023_Worksheets STUDENT/Assets/02_MATRICES_worksheet/Sonic/HMatrix2D.cs
@@ -6,50 +6,97 @@ public class HMatrix2D
 {
     public float[,] entries { get; set; } = new float[3, 3];
 
-    public HMatrix2D()
+    public HMatrix2D(float _x, float _y)
     {
-        // your code here
+        x = _x;
+        y = _y;
+        h = 1.0f;
     }
 
     public HMatrix2D(float[,] multiArray)
     {
-        // your code here
+         for (int y = 0; y < 3; y++) //loop through the rows y from 0 to < 3
+        {
+            for (int x = 0; x < 3; x++) //loop through the column x ffrom 0 to < 3
+            {
+                Entries[y, x] = multiArray[y, x]; //assigns the value from multiArray and puts them into Entries. This fills up the new matrix with the given numbers.
+            }
     }
+}
 
     public HMatrix2D(float m00, float m01, float m02,
              float m10, float m11, float m12,
              float m20, float m21, float m22)
     {
-	// First row
-        // your code here
+        Entries[0, 0] = m00;
+        Entries[0, 0] = m01;
+        Entries[0, 0] = m02;
 
-        // Second row
-        // your code here
+        Entries[0, 0] = m10;
+        Entries[0, 0] = m11;
+        Entries[0, 0] = m12;
 
-        // Third row
-        // your code here
+        Entries[0, 0] = m20;
+        Entries[0, 0] = m21;
+        Entries[0, 0] = m22;
+	
     }
 
     public static HMatrix2D operator +(HMatrix2D left, HMatrix2D right)
     {
-        return // your code here
+        HMatrix2D result = new HMatrix2D(); //creates a new matrix to put the result
+
+        for (int y = 0; y < 3; y++) 
+        {
+            for (int x = 0; x < 3; x++)
+            {
+                result.Entries[y, x] = left.Entries[y, x] + right.Entries[y, x]; //this adds the values in the same position of both matrix and puts the result in the new matrix
+            }
+        }
+
+        return result;
     }
 
     public static HMatrix2D operator -(HMatrix2D left, HMatrix2D right)
     {
-        return // your code here
+        HMatrix2D result = new HMatrix2D();
+
+        for (int y = 0; y < 3; y++)
+        {
+            for (int x = 0; x < 3; x++)
+            {
+                result.Entries[y, x] = left.Entries[y, x] - right.Entries[y, x]; //subtracts the value of the elements in the same position of both matrix and puts result in new matrix
+            }
+        }
+
+        return result;
     }
 
-    public static HMatrix2D operator *(HMatrix2D left, float scalar)
+    public static HMatrix2D operator *(HMatrix2D matrix, float scalar)
     {
-        return // your code here
+        HMatrix2D result = new HMatrix2D();
+
+        for (int y = 0; y < 3; y++)
+        {
+            for (int x = 0; x < 3; x++)
+            {
+                result.Entries[y, x] = matrix.Entries[y, x] * scalar;
+            }
+        }
+
+        return result;
     }
+
 
     // Note that the second argument is a HVector2D object
     //
     public static HVector2D operator *(HMatrix2D left, HVector2D right)
     {
-        return // your code here
+        return new HVector2D
+        (
+            left.Entries[0, 0] * right.x + left.Entries[0, 1] * right.y + left.Entries[0, 2] * 1.0f,
+            left.entries[1, 0] * right.x + left.Entries[1, 1] * right.y + left.Entries[1, 2] * 1.0f
+        );
     }
 
     // Note that the second argument is a HMatrix2D object
@@ -71,20 +118,36 @@ public class HMatrix2D
             xx xx xx    xx 21 xx
             */
             left.Entries[0, 0] * right.Entries[0, 1] + left.Entries[0, 1] * right.Entries[1, 1] + left.Entries[0, 2] * right.Entries[2, 1],
+            left.Entries[0, 0] * right.Entries[0, 2] + left.Entries[0, 1] * right.Entries[1, 2] + left.Entries[0, 2] * right.Entries[2, 2],
 
-	    // and so on for another 7 entries
-	);
+            left.Entries[1, 0] * right.Entries[0, 0] + left.Entries[1, 1] * right.Entries[1, 0] + left.Entries[1, 2] * right.Entries[2, 0],
+            left.Entries[1, 0] * right.Entries[0, 1] + left.Entries[1, 1] * right.Entries[1, 1] + left.Entries[1, 2] * right.Entries[2, 1],
+            left.Entries[1, 0] * right.Entries[0, 2] + left.Entries[1, 1] * right.Entries[1, 2] + left.Entries[1, 2] * right.Entries[2, 2],
+
+            left.Entries[2, 0] * right.Entries[0, 0] + left.Entries[2, 1] * right.Entries[1, 0] + left.Entries[2, 2] * right.Entries[2, 0],
+            left.Entries[2, 0] * right.Entries[0, 1] + left.Entries[2, 1] * right.Entries[1, 1] + left.Entries[2, 2] * right.Entries[2, 1],
+            left.Entries[2, 0] * right.Entries[0, 2] + left.Entries[2, 1] * right.Entries[1, 2] + left.Entries[2, 2] * right.Entries[2, 2] 
+            
+	   );
     }
 
     public static bool operator ==(HMatrix2D left, HMatrix2D right)
     {
-        // your code here
-    }
+        for (int y = 0; y < 3; y++)
+            for (int x = 0; x < 3; x++)
+                if (left.Entries[y, x] != right.Entries[y, x]) //checks if left and right is not equal
+                    return false; //if not equal, return false means matrices are not equal
+        return true; //otherwise return true means matrices are equal
+}
 
     public static bool operator !=(HMatrix2D left, HMatrix2D right)
     {
-        // your code here
-    }
+        for (int y = 0; y < 3; y++)
+            for (int x = 0; x < 3; x++)
+                if (left.Entries[y, x] != right.Entries[y, x]) //checks if left and right is not equal
+                    return true; //if not equal, return true means matrices are not equal
+        return false; //otherwise return false meaning matrices are equal
+}
 
     public override bool Equals(object obj)
     {
@@ -96,32 +159,63 @@ public class HMatrix2D
         // your code here
     }
 
-    public HMatrix2D transpose()
+    public HMatrix2D Transpose()
     {
-        return // your code here
+        //return // your code here
     }
 
-    public float getDeterminant()
+    public float GetDeterminant()
     {
-        return // your code here
+        //return // your code here
     }
 
-    public void setIdentity()
+    public void SetIdentity()
     {
-        // your code here
+    for (int y = 0; y < 3; y++) //loop through the rows y from 0 to < 3
+    {
+        for (int x = 0; x < 3; x++) //loop through the column x ffrom 0 to < 3
+        {
+            Entries[y, x] = (x == y) ? 1 : 0; //if the column x is equal to the rows y, set it to 1, if not, set to 0
+        }
+    }
+}
+//{
+    //for (int y = 0; y < 3; y++)    //loop through the rows y from 0 to < 3
+    //{
+        //for (int x = 0; x < 3; x++)    // loop through the column x from 0 to < 3
+        //{
+            //if (x == y)    //checks if x=y
+            //{
+                //Entries[y, x] = 1;    //if true, set y and x = 1
+            //}
+            //else
+            //{
+                //Entries[y, x] = 0;    //if false, set y and x = 0
+            //}
+        //}
+    //}
+//}
+
+    public void SetTranslationMat(float transX, float transY)
+    {
+        Entries[0, 2] = transX; //set translation in X direction
+        Entries[1, 2] = transY; //set translation in Y direction
     }
 
-    public void setTranslationMat(float transX, float transY)
+    public void SetRotationMat(float rotDeg)
     {
-        // your code here
-    }
+        //  cos x  -sin x
+        //  sin x   cos x
 
-    public void setRotationMat(float rotDeg)
-    {
-        // your code here
-    }
+        SetIdentity();
+        float rad = rotDeg * Mathf.Deg2Rad; //converts rotation degree to radians
+        Entries[0, 0] = Mathf.Cos(rad); //assigns the cos of the calculated radian angle to [0, 0]
+        Entries[0, 1] = -Mathf.Sin(rad); //assigns the -sin of the calculated radian angle to [0, 1]
+        Entries[1, 0] = Mathf.Sin(rad); //assigns the sin of the calculated radian angle to [1, 0]
+        Entries[1, 1] = Mathf.Cos(rad); //assigns the cos of the calculated radian angle to [1, 1]
+}
 
-    public void setScalingMat(float scaleX, float scaleY)
+    public void SetScalingMat(float scaleX, float scaleY)
     {
         // your code here
     }
--- GMAPS_Oct_2023_Worksheets STUDENT/Assets/02_MATRICES_worksheet/Sonic/TransformMesh.cs
+++ GMAPS_Oct_2023_Worksheets STUDENT/Assets/02_MATRICES_worksheet/Sonic/TransformMesh.cs
@@ -1,57 +1,61 @@
-﻿//// Uncomment this whole file.
+﻿using Mono.Cecil.Cil;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-//using Mono.Cecil.Cil;
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
+  public class TransformMesh : MonoBehaviour
+{
+    [HideInInspector]
+    public Vector3[] vertices { get; private set; }
 
-//public class TransformMesh : MonoBehaviour
-//{
-//    [HideInInspector]
-//    public Vector3[] vertices { get; private set; }
+    private HMatrix2D transformMatrix = new HMatrix2D();
+    HMatrix2D toOriginMatrix = new HMatrix2D();
+    HMatrix2D fromOriginMatrix = new HMatrix2D();
+    HMatrix2D rotateMatrix = new HMatrix2D();
 
-//    private HMatrix2D transformMatrix = new HMatrix2D();
-//    HMatrix2D toOriginMatrix = new HMatrix2D();
-//    HMatrix2D fromOriginMatrix = new HMatrix2D();
-//    HMatrix2D rotateMatrix = new HMatrix2D();
+    private MeshManager meshManager;
+    HVector2D pos = new HVector2D();
 
-//    private MeshManager meshManager;
-//    HVector2D pos = new HVector2D();
+    void Start()
+    {
+        meshManager = GetComponent<MeshManager>();
+        pos = new HVector2D(gameObject.transform.position.x, gameObject.transform.position.y);
 
-//    void Start()
-//    {
-//        meshManager = GetComponent<MeshManager>();
-//        pos = new HVector2D(gameObject.transform.position.x, gameObject.transform.position.y);
+        // Your code here
+    }
 
-//        // Your code here
-//    }
+    void Translate(float x, float y)
+    {
+        translateMatrix.SetIdentity(); //sets translation matrix to an identity matrix.
+        translateMatrix.SetTranslationMat(x, y); //configures the translation matrix to perform translation
+        Transform(); //calls the function transform
 
+        pos = translateMatrix * pos; //transforms the pos vector using translateMatrix
+    }
 
-//    void Translate(float x, float y)
-//    {
-//        // Your code here
-//    }
+    void Rotate(float angle)
+    {
+        transformMatrix.SetIdentity();
 
-//    void Rotate(float angle)
-//    {
-//        transformMatrix.SetIdentity();
+        // Your code here
 
-//        // Your code here
+        transformMatrix = fromOriginMatrix * // Your code here;
 
-//        transformMatrix = fromOriginMatrix * // Your code here;
+        Transform();    
+}
 
-//        Transform();
-//    }
+    private void Transform()
+    {
+        vertices = meshManager.clonedMesh.vertices;
 
-//    private void Transform()
-//    {
-        //vertices = meshManager.clonedMesh.vertices;
+        for (int i = 0; i < vertices.Length; i++) //loops through each vertex in the vertices array
+        {
+            HVector2D vert = new HVector2D(vertices[i].x,vertices[i].y); //creates a new 2D vector using x and y coordinates of the current vertex
+            vert = transformMatrix * vert; //transforms current vertex by multiplying it with trnasformMatrix
+            vertices[i].x = vert.x; //updates the x coordinate to the transformed x cocordinate
+            vertices[i].y = vert.y; //updates the y coordinate to the transformed y coordinate
+        }
 
-        //for (int i = 0; i < vertices.Length; i++)
-        //{
-        //    // Your code here
-        //}
-
-        //meshManager.clonedMesh.vertices = vertices;
-//    }
-//}
+        meshManager.clonedMesh.vertices = vertices;
+    }
+}
