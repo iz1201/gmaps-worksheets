087e691dc7a105ca9b1d4edb52a946bc4a7847df
iz1201 <isaaclzr123@hotmail.com>
2023-12-06 21:56:32 +0800

    FirstLaw.cs Motion.cs JumpToHeight.cs update

--- GMAPS_Oct_2023_Worksheets STUDENT/Assets/03_KINEMATICS_Worksheet/Part 1/Force/Motion.cs
+++ GMAPS_Oct_2023_Worksheets STUDENT/Assets/03_KINEMATICS_Worksheet/Part 1/Force/Motion.cs
@@ -1,19 +1,19 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// public class Motion : MonoBehaviour
-// {
-//     public Vector3 Velocity;
+public class Motion : MonoBehaviour
+{
+    public Vector3 Velocity;
 
-//     void FixedUpdate()
-//     {
-//         float dt = Time.deltaTime;
+    void FixedUpdate()
+    {
+        float dt = Time.deltaTime; 
 
-//         float dx = /*your code here*/ * dt;
-//         float dy = /*your code here*/;
-//         float dz = /*your code here*/;
+        float dx = Velocity.x * dt; //calculate the displacement along the x-axis by multiplying velocity.x and dt
+        float dy = Velocity.y * dt; //calculate the displacement along the y-axis by multiplying velocity.y and dt
+        float dz = Velocity.z * dt; //calculate the displacement along the z-axis by multiplying velocity.z and dt
 
-//         transform./*your code here*/(new Vector3(/*your code here*/));
-//     }
-// }
+        transform.Translate(new Vector3(dx, dy, dz)); //moves the object position by the calculated displacements along each axis by using transform.Translate
+    }
+}
--- GMAPS_Oct_2023_Worksheets STUDENT/Assets/03_KINEMATICS_Worksheet/Part 1/JumpToHeight/JumpToHeight.cs
+++ GMAPS_Oct_2023_Worksheets STUDENT/Assets/03_KINEMATICS_Worksheet/Part 1/JumpToHeight/JumpToHeight.cs
@@ -1,37 +1,35 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// public class JumpToHeight : MonoBehaviour
-// {
-//     public float Height = 1f;
-//     Rigidbody rb;
+public class JumpToHeight : MonoBehaviour
+{
+     public float Height = 1f;
+     Rigidbody rb;
 
-//     private void Start()
-//     {
-//         rb = /*your code here*/;
-//     }
+     private void Start()
+     {
+         rb = GetComponent<Rigidbody>(); //get rigid body component
+     }
 
-//     void Jump()
-//     {
-//         // v*v = u*u + 2as
-//         // u*u = v*v - 2as
-//         // u = sqrt(v*v - 2as)
-//         // v = 0, u = ?, a = Physics.gravity, s = Height
+     void Jump()
+     {
+          //v*v = u*u + 2as
+          //u*u = v*v - 2as
+          //u = sqrt(v*v - 2as)
+          //v = 0, u = ?, a = Physics.gravity, s = Height
 
-//         float u = Mathf.Sqrt(/*your code here*/);
-//         rb.velocity = new Vector3(/*your code here*/);
+         float u = Mathf.Sqrt(-2 * Physics.gravity.y * Height); //calculates the initial velocity u
+         rb.velocity = new Vector3(0, u, 0); //sets rigidbody velocity to make object jump with 'u' velocity in the y axis
 
-//         //float jumpForce = Mathf.Sqrt(-2 * Physics2D.gravity.y * Height);
-//         //rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
-//     }
+     }
 
-//     private void Update()
-//     {
-//         if(Input.GetKeyDown(KeyCode.Space))
-//         {
-//             /*your code here*/();
-//         }
-//     }
-// }
+     private void Update()
+     {
+         if(Input.GetKeyDown(KeyCode.Space)) //checks if space is press
+         {
+             Jump(); //calls the jump method
+         }
+     }
+ }
 
